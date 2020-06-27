0223791918696f23460dc6b6ea5570b71e3115ca
fanning-968 <1552730303@qq.com>
2020-06-27 12:24:54 +0800

    樊宁提交

--- src/main/java/com/fanning/github/HelloCighub.java
+++ src/main/java/com/fanning/github/HelloCighub.java
@@ -5,6 +5,10 @@ public class HelloCighub {
     public static void main(String[] args) {
 
         System.out.println("Hello Github 版本01");
+        System.out.println("俺就导航");
+        System.out.println("买那个");
+        System.out.println("马上快点喝 版本01");
+        System.out.println("mker sw 版本01");
 
 
     }
